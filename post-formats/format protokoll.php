381844bbbff9bf3963b6d58aa2e5a3544ed84b30
Felix Schneider <felix.schneider@wortundbildverlag.de>
2017-03-03 17:37:47 +0100

    Protokolle template

--- post-formats/format protokoll.php
+++ post-formats/format protokoll.php
@@ -1,17 +1,15 @@
 
 
                 <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
-                    
-                <?php $url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false ); ?>    
-                 
-                <div class="textContainer">   
-                    
-                <div class="post-header-image" style="background-image:url(<?php echo $url[0]; ?>)"></div>     
 
                 <header class="post-header">
                     
+                <?php $url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false ); ?>    
+                    
+                <div class="post-header-image" style="background-image:url(<?php echo $url[0]; ?>)"></div> 
+                    
                 <div class="headerText">  
-                <h3>Protokolle der Angst</h3>        
+
                 <h1 class="post-title"><?php the_title(); ?></h1>
                 <span class="excerpt"><?php the_excerpt(); ?></span>    
                 <span class="author">von <?php the_cfc_field('autoren', 'autor'); ?></span>    
@@ -24,7 +22,5 @@
                 <?php the_content();?>
                     
                 </section> <?php // end article section ?>
-                    
-                </div>     
 
                 </article> <?php // end article ?>
